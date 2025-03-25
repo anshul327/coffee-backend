f47d0c4b33eff94553e93354e9673c28ddf10431
Anshul Chawla <anshulnitb26@gmail.com>
2025-03-25 21:21:05 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // NODEMON is used to reload so that we dont have to restart again n again after saving changes in backend file
+
+// require('dotenv').config({path : './env'}) OR USE
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
